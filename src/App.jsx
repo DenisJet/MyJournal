84dc91ca139c27f3c35accf6a6fa4c23e78bcfc7
Denis Kalkopf <deniskalkopf@gmail.com>
2024-04-04 19:55:52 +0500

    add Button

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import './App.css';
+import Button from './components/Button/Button';
 
 function App() {
-  return <div>Project</div>;
+  return (
+    <>
+      <div>Project</div> 
+      <Button/>
+    </>
+  );
 }
 
 export default App;
