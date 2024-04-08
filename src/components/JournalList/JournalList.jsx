6680554767043cad32c469b3a47a098ee49a31b2
Denis Kalkopf <deniskalkopf@gmail.com>
2024-04-08 16:42:23 +0500

    add useMemo

--- src/components/JournalList/JournalList.jsx
+++ src/components/JournalList/JournalList.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useMemo } from 'react';
 import CardButton from '../CardButton/CardButton';
 import JournalItem from '../JournalItem/JournalItem';
 import './JournalList.css';
@@ -6,10 +6,6 @@ import { UserContext } from '../../context/user.context';
 
 export default function JournalList({ items }) {
   const { userId } = useContext(UserContext);
-
-  if (items.length === 0) {
-    return <p>Записей пока нет, добавьте первую</p>;
-  }
 
   const sortItems = (a, b) => {
     if (a.date > b.date) {
@@ -19,16 +15,19 @@ export default function JournalList({ items }) {
     }
   };
 
+  const filteredItems = useMemo(() => items.filter((el) => el.userId === userId).sort(sortItems), [items, userId]);
+
+  if (items.length === 0) {
+    return <p>Записей пока нет, добавьте первую</p>;
+  }
+
   return (
     <>
-      {items
-        .filter((el) => el.userId === userId)
-        .sort(sortItems)
-        .map((el) => (
-          <CardButton key={el.id}>
-            <JournalItem title={el.title} text={el.text} date={el.date} />
-          </CardButton>
-        ))}
+      {filteredItems.map((el) => (
+        <CardButton key={el.id}>
+          <JournalItem title={el.title} text={el.text} date={el.date} />
+        </CardButton>
+      ))}
     </>
   );
 }
