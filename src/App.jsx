e8699b226549c6bbf990857eefa8c788f4b76408
Denis Kalkopf <deniskalkopf@gmail.com>
2024-04-08 14:07:46 +0500

    add custom hook useLocalStorage

--- src/App.jsx
+++ src/App.jsx
@@ -6,46 +6,44 @@ import JournalForm from './components/JournalForm/JournalForm';
 import JournalList from './components/JournalList/JournalList';
 import Body from './layouts/Body/Body';
 import LeftPanel from './layouts/LeftPanel/LeftPanel';
+import { useLocalStorage } from './hooks/use-localstorage.hook';
+
+function mapItems(items) {
+  if (!items) {
+    return [];
+  }
+
+  return items.map((i) => ({
+    ...i,
+    date: new Date(i.date),
+  }));
+}
 
 function App() {
-  const [items, setItems] = useState([]);
-
-  useEffect(() => {
-    const data = JSON.parse(localStorage.getItem('data'));
-
-    if (data) {
-      setItems(data.map(item => ({
-        ...item,
-        date: new Date(item.date)
-      })))
-    }
-  }, []);
-
-  useEffect(() => {
-    if (items.length) {
-      localStorage.setItem('data', JSON.stringify(items));
-    }
-  }, [items])
+  const [items, setItems] = useLocalStorage('data');
 
   const addItem = (item) => {
-    setItems(oldItems => [...oldItems, {
-      id: oldItems.length > 0 ? Math.max(...oldItems.map(i => i.id)) + 1 : 1,
-      text: item.text,
-      title: item.title,
-      date: new Date(item.date)
-    }])
-  }
- 
+    setItems([
+      ...mapItems(items),
+      {
+        id: items.length > 0 ? Math.max(...items.map((i) => i.id)) + 1 : 1,
+        text: item.text,
+        title: item.title,
+        date: new Date(item.date),
+      },
+    ]);
+  };
+
   return (
     <div className='app'>
-    <LeftPanel>
-      <Header/>
-      <JournalAddButton/>
-      <JournalList items={items}/>
-    </LeftPanel>
-    <Body>
-      <JournalForm onSubmit={addItem}/>
-    </Body>
+      <LeftPanel>
+        <Header />
+        <JournalAddButton />
+        <JournalList items={mapItems(items)} />
+      </LeftPanel>
+      <Body>
+        <JournalForm onSubmit={addItem} />
+      </Body>
     </div>
   );
 }
